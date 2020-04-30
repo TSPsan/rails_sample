e6281154cf5dbd40026fb9c86554c09d02bc161f
TSP <vsig7fs42v8a@gmail.com>
2020-04-30 20:05:02 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,6 +5,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+	def setup
+    @user = users(:michael)
+	end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -12,6 +16,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,           count: 0
+    assert_select "a[href=?]", users_path,            count: 0
+    assert_select "a[href=?]", user_path(@user),      count: 0
+    assert_select "a[href=?]", edit_user_path(@user), count: 0
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
